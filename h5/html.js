4427ec221d658425d74ce9408ca70beaed7dd1c6
Rishabh Jain <therishabh16@gmail.com>
2021-03-07 23:10:28 +0530

    Update html.js

--- h5/html.js
+++ h5/html.js
@@ -5,62 +5,56 @@ const content = script =>
   <meta charset="utf-8">
   <title>Signature Pad demo</title>
   <meta name="description" content="Signature Pad - HTML5 canvas based smooth signature drawing using variable width spline interpolation.">
-
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
-
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
-
   <style>
+  @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&display=swap');
     body {
-      font-family: Helvetica, Sans-Serif;
     
       -moz-user-select: none;
       -webkit-user-select: none;
       -ms-user-select: none;
-      margin:0;
-      overflow:hidden;
-    }
-    body,html {
-      width: 100%;
-      height: 300px;
-    }
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
     }
     
-    .rotated-true {
-      transform: rotate(90deg);
-      transform-origin:bottom left;
-      
-      position:absolute;
-      top: -100vw;
-      left: 0;
-      
-      height:100vw;
-      width:100vh;
-      
-      overflow:auto;
-    }
-    .rotated-false {
-      width: 100%;
-      height: 100%;
-    }
     .m-signature-pad {
+      position: absolute;
       font-size: 10px;
-      width: 100%;
-      height: 100%;
-      border: 1px solid #e8e8e8;
+      width: 700px;
+      height: 400px;
+      top: 50%;
+      left: 50%;
+      margin-left: -350px;
+      margin-top: -200px;
       background-color: #fff;
       box-shadow: 0 1px 4px rgba(0, 0, 0, 0.27), 0 0 40px rgba(0, 0, 0, 0.08) inset;
     }
     
-    .m-signature-pad--body {
-      position: relative;
-      height: 100%;
-      border: 1px solid #f4f4f4;
+    .m-signature-pad:before, .m-signature-pad:after {
+      position: absolute;
+      z-index: -1;
+      content: "";
+      width: 40%;
+      height: 10px;
+      left: 20px;
+      bottom: 10px;
+      background: transparent;
+      -webkit-transform: skew(-3deg) rotate(-3deg);
+      -moz-transform: skew(-3deg) rotate(-3deg);
+      -ms-transform: skew(-3deg) rotate(-3deg);
+      -o-transform: skew(-3deg) rotate(-3deg);
+      transform: skew(-3deg) rotate(-3deg);
+      box-shadow: 0 8px 12px rgba(0, 0, 0, 0.4);
+    }
+    
+    .m-signature-pad:after {
+      left: auto;
+      right: 20px;
+      -webkit-transform: skew(3deg) rotate(3deg);
+      -moz-transform: skew(3deg) rotate(3deg);
+      -ms-transform: skew(3deg) rotate(3deg);
+      -o-transform: skew(3deg) rotate(3deg);
+      transform: skew(3deg) rotate(3deg);
     }
     
     .m-signature-pad--body
@@ -74,49 +68,48 @@ const content = script =>
         box-shadow: 0 0 5px rgba(0, 0, 0, 0.02) inset;
       }
     
-    .m-signature-pad--footer {
-      padding: 0px 20px;
-      margin-top: 10px;
-      position: relative;
-      height: 40px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-    }
-    
-    .m-signature-pad--footer
-      .description {
-        color: #C3C3C3;
-        text-align: center;
-        font-size: 1.2em;
+      .m-signature-pad--footer{
+        width: 100%;
       }
-    
     .m-signature-pad--footer
       .button {
-        background-color: #3F99F7;
-        height: 32px;
-        padding: 0 20px;
-        line-height: 32px;
+        position: absolute;
+        bottom: 20px;
+        height: 40px;
+        padding: 0 45px;
         text-align: center;
         color: #FFF;
         border: 1px solid transparent;
         border-radius: 4px;
         outline: none;
-        box-shadow: none;
+        box-shadow: -4.41667px -4.41667px 13.25px #FFFFFF, 6.625px 6.625px 13.25px rgba(174, 174, 192, 0.4);;
+        font-size: 1.6em;
+        font-family: Comfortaa;
       }
     
     .m-signature-pad--footer
       .button.clear {
+        left: -20px;
+        background-color: #B7544A;
       }
     
     .m-signature-pad--footer
       .button.save {
+        right: -20px;
+        background-color: #1E96A3;
       }
     
     @media screen and (max-width: 1024px) {
       .m-signature-pad {
-        width: 100%;
-        height: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: auto;
+        height: auto;
+        min-width: 250px;
+        min-height: 140px;
+        margin: 0;
       }
       #github {
         display: none;
@@ -142,29 +135,20 @@ const content = script =>
         bottom: 4px;
         height: 28px;
       }
-      .m-signature-pad--footer
-        .description {
-          font-size: 1em;
-          margin-top: 1em;
-        }
     }
     <%style%>
     </style>
 </head>
 <body onselectstart="return false">
-<div class="rotated-<%orientation%>">
-    <div id="signature-pad" class="m-signature-pad">
-      <div class="m-signature-pad--body">
-        <canvas></canvas>
-      </div>
-      <div class="m-signature-pad--footer">
-        <button type="button" class="button clear" data-action="clear"><%clear%></button>
-        <div class="description"><%description%></div>
-        <button type="button" class="button save" data-action="save"><%confirm%></button>
-      </div>
+  <div id="signature-pad" class="m-signature-pad">
+    <div class="m-signature-pad--body">
+      <canvas></canvas>
+    </div>
+    <div class="m-signature-pad--footer">
+      <button type="button" class="button clear" data-action="clear"><%clear%></button>
+      <button type="button" class="button save" data-action="save"><%confirm%></button>
     </div>
   </div>
-
   <script>
     ${script}
   </script>
